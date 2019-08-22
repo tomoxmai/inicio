b236258e095045786a6bcc315f78e5089d4f4b3a
nestor lavariega <altair1970@hotmail.es>
2019-08-21 20:46:14 -0500

    pompoim

---
+++ loginn/loginn/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace loginn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nombres = new System.Windows.Forms.Label();
+            this.contraseñas = new System.Windows.Forms.Label();
+            this.nombre = new System.Windows.Forms.TextBox();
+            this.contraseña = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Minya Nouvelle", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.button1.Location = new System.Drawing.Point(606, 402);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "INICIAR!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(514, 367);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Minya Nouvelle", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-1, -10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(542, 78);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "POMPOM LAND";
+            // 
+            // nombres
+            // 
+            this.nombres.AutoSize = true;
+            this.nombres.Font = new System.Drawing.Font("Minya Nouvelle", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nombres.ForeColor = System.Drawing.Color.Chocolate;
+            this.nombres.Location = new System.Drawing.Point(557, 73);
+            this.nombres.Name = "nombres";
+            this.nombres.Size = new System.Drawing.Size(142, 20);
+            this.nombres.TabIndex = 3;
+            this.nombres.Text = "Usuario o correo ";
+            // 
+            // contraseñas
+            // 
+            this.contraseñas.AutoSize = true;
+            this.contraseñas.Font = new System.Drawing.Font("Minya Nouvelle", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contraseñas.ForeColor = System.Drawing.Color.Chocolate;
+            this.contraseñas.Location = new System.Drawing.Point(557, 146);
+            this.contraseñas.Name = "contraseñas";
+            this.contraseñas.Size = new System.Drawing.Size(96, 20);
+            this.contraseñas.TabIndex = 4;
+            this.contraseñas.Text = "Contraseña";
+            this.contraseñas.Click += new System.EventHandler(this.Contraseña_Click);
+            // 
+            // nombre
+            // 
+            this.nombre.Location = new System.Drawing.Point(561, 96);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(223, 20);
+            this.nombre.TabIndex = 5;
+            this.nombre.TextChanged += new System.EventHandler(this.Nombre_TextChanged);
+            // 
+            // contraseña
+            // 
+            this.contraseña.Location = new System.Drawing.Point(561, 169);
+            this.contraseña.Name = "contraseña";
+            this.contraseña.Size = new System.Drawing.Size(223, 20);
+            this.contraseña.TabIndex = 6;
+            this.contraseña.TextChanged += new System.EventHandler(this.Contraseña_TextChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(561, 195);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(223, 213);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.contraseña);
+            this.Controls.Add(this.nombre);
+            this.Controls.Add(this.contraseñas);
+            this.Controls.Add(this.nombres);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label nombres;
+        private System.Windows.Forms.Label contraseñas;
+        private System.Windows.Forms.TextBox nombre;
+        private System.Windows.Forms.TextBox contraseña;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ loginn/loginn/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace loginn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string usuario = nombre.text;
+            string contra = contraseña.text;
+            if (usuario == "juancho"&&contra=="123frijoles")
+            {
+                MessageBox.Show("Bienvenido a Pompom Land! (●´ω｀●)");
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Acceso denegado. (￣□￣) ")
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Contraseña_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Nombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Contraseña_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
